96f004abeb6644583303c2f5b881efcc6b45f1ac
mecharis-420 <121699952+mecharis-420@users.noreply.github.com>
2023-09-17 13:18:50 -0400

    f

---
+++ cloaker.js
@@ -0,0 +1,32 @@
+let inFrame
+
+try {
+    inFrame = window !== top
+} catch (e) {
+    inFrame = true
+}
+
+if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+    const popup = open("about:blank", "_blank")
+    if (!popup || popup.closed) {
+        alert("Allow popups and redirects to hide this from showing up in your history.")
+    } else {
+        const doc = popup.document
+        const iframe = doc.createElement("iframe")
+        const style = iframe.style
+        const link = doc.createElement("link")
+
+        doc.title = "Classes"
+        link.rel = "icon";
+        link.href = "https://upload.wikimedia.org/wikipedia/commons/5/59/Google_Classroom_Logo.png";
+        iframe.src = location.href
+        style.position = "fixed"
+        style.top = style.bottom = style.left = style.right = 0
+        style.border = style.outline = "none"
+        style.width = style.height = "100%"
+
+        doc.body.appendChild(iframe)
+        location.replace("https://classroom.google.com/")
+    }
+}
+
