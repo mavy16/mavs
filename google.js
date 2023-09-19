4f252beeb5636837e94f67e88f25e35b07041d51
mecharis-420 <121699952+mecharis-420@users.noreply.github.com>
2023-09-19 16:09:18 -0400

    ok

--- google.js
+++
@@ -1,9 +0,0 @@
-function google() {
-
-    window.navigator.serviceWorker.register('/sw.js', {
-        scope: __uv$config.prefix
-    }).then(() => {
-        location.href=__uv$config.prefix + __uv$config.encodeUrl('https://google.com');
-    });
-
-}
--- sw.js
+++
@@ -1,7 +0,0 @@
-importScripts('./uv/uv.sw.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', event => {
-  if (event.request.url.startsWith(location.origin+'/service/uv/')) event.respondWith(sw.fetch(event));
-});
